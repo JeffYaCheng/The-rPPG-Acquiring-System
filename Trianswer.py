f6ffa397df8e5351085de2a9e7c30343393f5140
Jeff <e24084127@gs.ncku.edu.tw>
2024-02-04 16:27:48 +0800

    large view and add acquiring system

--- Trianswer.py
+++ Trianswer.py
@@ -16,6 +16,13 @@ import json
 import math
 from tkinter import messagebox
 
+import cv2
+import time
+from tkinter import *
+import PIL
+from PIL import ImageTk
+from PIL import Image
+
 DATA_VAL_UUID = "0000a001-0000-1000-8000-00805f9b34fb"
 DEVICE_NAME_FILTER = "Tri"
 Device_SN_flag = True
@@ -37,6 +44,13 @@ CH_NUM = 3
 PPG_VALID_LEN_INDEX = 72
 frm_Ch = [None, None, None]
 
+#record video
+camera=None
+video=None
+image_width=int(640)
+image_height=int(480)
+camera_num=int(1)
+
 # File related
 TriAnswer_PROG_ROOT = os.path.abspath(os.getcwd())
 TriAnswer_Record_Dir = os.path.join(TriAnswer_PROG_ROOT, 'TriAnswer_Records')
@@ -52,6 +66,7 @@ FLOW_CTRL_STATE :
 '''
 FLOW_CTRL_STATE = 0
 
+
 # TK new UI
 Log_text_val = ""
 Btn_BLE_control_color = ["#5CADAD", "#02C874", "#FF2D2D"]
@@ -60,7 +75,7 @@ Signal_FRAME_LEN = waveform_buffer_len / freq
 waveform_buffer = np.zeros((CH_NUM+1, waveform_buffer_len))
 waveform_buffer[waveform_buffer == 0] = np.nan
 waveform_buffer_index = np.zeros(CH_NUM+1, int)
-waveform_draw_period = 500
+waveform_draw_period = 1000
 PPG_DC_GET_FLAG = False
 PPG_DC = [0,0]
 
@@ -85,7 +100,6 @@ def FLOW_CTRL_BTN_action():
         print('> Start scanning ...')
         log_update('> Start scanning ...')
         threading.Thread(target=_scan_thread, args=(async_loop,)).start()
-
     if FLOW_CTRL_STATE == 1:  # Try to connect to device
         log_update('> Start connecting ...')
 
@@ -97,7 +111,8 @@ def FLOW_CTRL_BTN_action():
         FLAG_SENSING_EN = True
         print(f'> Start connecting {Target_device.address} ...')
         threading.Thread(target=_connect_to_device_thread, args=(async_loop,)).start()
-
+        threading.Thread(target= turn_on_camera).start() # turn on camera
+        threading.Thread(target= waveform_draw_task).start() #draw waveform
     if FLOW_CTRL_STATE == 2:  # Try to disconnect to device
         FLAG_SENSING_EN = False
         FLOW_CTRL_STATE = 0
@@ -187,9 +202,12 @@ async def handle_rx_data(_: int, data: bytearray):
                 wave_temp_array[ch_ind] = np.repeat(PPG_temp_arr[ch_ind - 2],CH_SpeedMode[ch_ind].get())-PPG_DC[ch_ind - 2]
 
             # Write to file
-            if Btn_Record_State == 1:
+            if Btn_Record_State == 1: ## when record bottom trigger and the function change the state it will start write  txt file
+                #start_recording_2 = time.time() ## test thread 
+                #print("start_save_file",start_recording_2)
                 for ele in wave_temp_array[ch_ind][::CH_SpeedMode[ch_ind].get()]:
                     TA_FileHandle[ch_ind].write(f'{ele}\n')
+
 
         if (waveform_buffer_index[ch_ind] + len(wave_temp_array[ch_ind])) <= len(
                 waveform_buffer[ch_ind]):  # buffer not full
@@ -218,6 +236,7 @@ async def connect_to_device():
                 await client.get_services()
                 conn_connected = client.is_connected()
                 print("> Device connected !")
+                    
                 if conn_connected:
                     FLOW_CTRL_STATE = 2
                     Btn_Record['state'] = 'normal'
@@ -225,7 +244,7 @@ async def connect_to_device():
                     Btn_BLE_control['text'] = 'Stop Connection'
                     Btn_BLE_control['bg'] = Btn_BLE_control_color[FLOW_CTRL_STATE]
                     log_update("> Connect to TriAnswer and try to initiate the measurement ...")
-                    waveform_draw_task()
+                    #waveform_draw_task() #original draw waveform
 
                     await client.start_notify(DATA_VAL_UUID, handle_rx_data)
                     log_update('> Sensing ...')
@@ -245,7 +264,7 @@ async def connect_to_device():
     # After FLAG_SENSING_EN flag turns to False -> stop_notify and disconnect BLE device
     log_update(f'> [BLE] Try to disconnect...')
     await client.disconnect()
-    closeRecordFile()
+    closeRecordFile() # i think if disconnect and forget stop record will use it to close file
 
     if not FLAG_WINDOW_CLOSE:
         conn_connected = False
@@ -331,41 +350,189 @@ def log_update(input_str):
 
 
 def waveform_draw_task():
-    for ch_num in range(0, 3):
-        mat_ax[ch_num].clear()
+    global conn_connected, client, Target_device, MQTTClient, FLOW_CTRL_STATE, \
+        waveform_buffer, waveform_buffer_index, PPG_DC_GET_FLAG,FLAG_SENSING_EN
+    while(FLAG_SENSING_EN): 
+        for ch_num in range(0, 3):
+            mat_ax[ch_num].clear()
 
-        if ch_num < 2:
-            mat_ax[ch_num].plot(time_array[::CH_SpeedMode[ch_num].get()],
-                                waveform_buffer[ch_num][::CH_SpeedMode[ch_num].get()],
-                                color='r', linewidth=1, alpha=0.7)
+            if ch_num < 2:
+                mat_ax[ch_num].plot(time_array[::CH_SpeedMode[ch_num].get()],
+                                    waveform_buffer[ch_num][::CH_SpeedMode[ch_num].get()],
+                                    color='r', linewidth=1, alpha=0.7)
 
+            else:
+                # CH3
+                mat_ax[ch_num].plot(time_array[::CH_SpeedMode[ch_num].get()],
+                                    waveform_buffer[ch_num][::CH_SpeedMode[ch_num].get()],
+                                    color='r', linewidth=1, alpha=0.7)
+                # CH4
+                mat_ax[ch_num].plot(time_array[::CH_SpeedMode[ch_num + 1].get()],
+                                    waveform_buffer[ch_num + 1][::CH_SpeedMode[ch_num + 1].get()],
+                                    color='b', linewidth=1, alpha=0.7)
+
+            # axis setting
+            mat_ax[ch_num].set_xlim(0, Signal_FRAME_LEN)
+            mat_ax[ch_num].set_xticks(np.arange(0, Signal_FRAME_LEN, step=0.2))
+            mat_ax[ch_num].set_xticklabels([])
+            mat_ax[ch_num].grid(axis='x')
+            mat_ax[ch_num].yaxis.set_visible(False)
+            mat_ax[ch_num].set_yticks([])
+
+        # update figure
+        canvas.draw()
+        time.sleep(0.5)
+    #if FLOW_CTRL_STATE == 2:  # when connected
+    #    frm_fig.after(waveform_draw_period, waveform_draw_task) #一直循環call function 讓視頻閃爍
+
+# for video updata
+class ipcamCapture:
+    def __init__(self):
+        global camera_num
+        self.Frame = []
+        self.status = False
+        self.isstop = False
+        self.connect =False
+        # 攝影機連接 # 1 外接 
+        self.capture = cv2.VideoCapture(camera_num)
+        if not self.capture.isOpened():
+            print('just have one camera')
+            self.capture = cv2.VideoCapture(0)
+         
+        #關掉相機自動的功能
+        exp=self.capture.get(cv2.CAP_PROP_EXPOSURE)
+        wb=self.capture.get(cv2.CAP_PROP_WB_TEMPERATURE)
+        fc=self.capture.get(cv2.CAP_PROP_FOCUS)
+        self.capture.set(cv2.CAP_PROP_AUTO_WB,0) # 關掉自動白平衡 0
+        self.capture.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
+        self.capture.set(cv2.CAP_PROP_AUTOFOCUS , 0)
+        self.capture.set(cv2.CAP_PROP_EXPOSURE,exp)
+        self.capture.set(cv2.CAP_PROP_WB_TEMPERATURE,wb)
+        self.capture.set(cv2.CAP_PROP_FOCUS,fc)
+        
+        if not self.capture.isOpened():
+            print("Cannot open camera")
+            exit() 
         else:
-            # CH3
-            mat_ax[ch_num].plot(time_array[::CH_SpeedMode[ch_num].get()],
-                                waveform_buffer[ch_num][::CH_SpeedMode[ch_num].get()],
-                                color='r', linewidth=1, alpha=0.7)
-            # CH4
-            mat_ax[ch_num].plot(time_array[::CH_SpeedMode[ch_num + 1].get()],
-                                waveform_buffer[ch_num + 1][::CH_SpeedMode[ch_num + 1].get()],
-                                color='b', linewidth=1, alpha=0.7)
+            print("turn on camera")
+            self.connect=True
 
-        # axis setting
-        mat_ax[ch_num].set_xlim(0, Signal_FRAME_LEN)
-        mat_ax[ch_num].set_xticks(np.arange(0, Signal_FRAME_LEN, step=0.2))
-        mat_ax[ch_num].set_xticklabels([])
-        mat_ax[ch_num].grid(axis='x')
-        mat_ax[ch_num].yaxis.set_visible(False)
-        mat_ax[ch_num].set_yticks([])
+    def start(self):
+    # 把程式放進子執行緒，daemon=True 表示該執行緒會隨著主執行緒關閉而關閉。
+        print('ipcam started!')
+        threading.Thread(target=self.queryframe, daemon=True, args=()).start()
 
-    # update figure
-    canvas.draw()
+    def stop(self):
+    # 記得要設計停止無限迴圈的開關。
+        self.isstop = True
+        print('ipcam stopped!')
+        self.capture.release()
 
-    if FLOW_CTRL_STATE == 2:  # when connected
-        window.after(waveform_draw_period, waveform_draw_task)
+    def getframe(self):
+    # 當有需要影像時，再回傳最新的影像。
+        return self.Frame.copy()
 
+    def queryframe(self):
+        global video
+        while (not self.isstop):
+            self.status, self.Frame = self.capture.read()
+            if not self.status:
+                print("Can't receive frame (stream end?). Exiting ...")
+            else:
+                # 記錄影片
+                while(Btn_Record_State):
+                    self.status, self.Frame = self.capture.read()
+                    video.write(self.Frame)
+        self.capture.release()
 
+        
+def turn_on_camera():
+    print('enter function turn_on_camera')
+    global camera,video,FLAG_SENSING_EN,Btn_Record_State
+    ipcam = ipcamCapture()
+    ipcam.start()
+    #讓相機初始設定
+    while(not ipcam.connect):
+        time.sleep(1)
+ 
+    #只要是sensing 時，照相機也會一直開著
+    while(FLAG_SENSING_EN):
+        # 擷取影像
+        frame = ipcam.getframe()
+        if ipcam.status:
+            try:
+                frame = cv2.resize(frame, (image_width, image_height), interpolation=cv2.INTER_AREA) #有可能第一張收不到frame
+            except:
+                print('can not get frame')
+                continue
+            # resize image to 640*480
+            #frame = cv2.resize(frame, (image_width, image_height), interpolation=cv2.INTER_AREA)
+            frame = cv2.flip(frame, 1)    # 左右翻轉
+            #add rectangle
+            cv2.rectangle(frame, (int(5*frame.shape[1]/10+frame.shape[1]/20), int(frame.shape[0]/5)), (int(9*frame.shape[1]/10), int(4*frame.shape[0]/5)), (0, 0, 255), 2)
+            cv2.rectangle(frame, (int(1*frame.shape[1]/10), int(1*frame.shape[0]/6)), (int(5*frame.shape[1]/10), int(5*frame.shape[0]/6)), (0, 0, 255), 2)
+            # show frame on tk
+            frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            pilImage = Image.fromarray(frame)
+            imgtk=ImageTk.PhotoImage(image=pilImage)
+            movieLabel.config(image=imgtk)
+            movieLabel.image=imgtk
+            #canvas_v.delete("all") 
+            #canvas_v.create_image(0,0,anchor=NW,image=imgtk)
+            #canvas_v.image=imgtk
+            
+            start_recording = time.time()
+            while(Btn_Record_State ):
+                now_time=time.time()
+                frame = ipcam.getframe()
+                frame = cv2.resize(frame, (image_width, image_height), interpolation=cv2.INTER_AREA)
+                frame = cv2.flip(frame, 1)    # 左右翻轉
+                # 放時間
+                pos=(450,10)
+                x,y=pos
+                font= cv2.FONT_HERSHEY_PLAIN
+                font_scale=2
+                font_thickness=1
+                text=f'time{str(int(now_time-start_recording))}s'
+                text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+                text_w, text_h = text_size
+                cv2.rectangle(frame, pos, (x + text_w, y + text_h), (0,0,0), -1)
+                cv2.putText(frame, text, (x, y + text_h + font_scale - 1), font, font_scale, (255, 255, 255), font_thickness)
+                #放框框
+                cv2.rectangle(frame, (int(5*frame.shape[1]/10+frame.shape[1]/20), int(frame.shape[0]/5)), (int(9*frame.shape[1]/10), int(4*frame.shape[0]/5)), (0, 0, 255), 2)
+                cv2.rectangle(frame, (int(1*frame.shape[1]/10), int(1*frame.shape[0]/6)), (int(5*frame.shape[1]/10), int(5*frame.shape[0]/6)), (0, 0, 255), 2)
+                test=time.time()
+                 # show frame on tk
+                frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                pilImage = Image.fromarray(frame)
+                imgtk=ImageTk.PhotoImage(image=pilImage)
+                #canvas_v.delete("all") 
+                #canvas_v.create_image(0,0,anchor=NW,image=imgtk)
+                #canvas_v.image=imgtk
+                movieLabel.config(image=imgtk)
+                movieLabel.image=imgtk
+                out_time = time.time()
+                #print(f'total time = {out_time-now_time}')
+                #print(f'tk time = {out_time-test}')
+            
+    # 釋放該攝影機裝置
+    print('turn off camera')
+    ipcam.stop()
+    print('leave function turn_on_camera')
+    #放logo
+    frame = cv2.imread("./logo.jpg")
+    frame = cv2.resize(frame, (image_width, image_height), interpolation=cv2.INTER_AREA)
+    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    pilImage = Image.fromarray(frame)
+    imgtk=ImageTk.PhotoImage(image=pilImage)
+    #canvas_v.delete("all") 
+    #canvas_v.create_image(0,0,anchor=NW,image=imgtk)
+    #canvas_v.image=imgtk
+    movieLabel.config(image=imgtk)
+    movieLabel.image=imgtk    
+    
 def createRecordFile():
-    global TA_FileHandle
+    global TA_FileHandle,video ##add global variable video
 
     dirIsExist = os.path.exists(TriAnswer_Record_Dir)
     if not dirIsExist:
@@ -380,21 +547,28 @@ def createRecordFile():
             childView.configure(state='disable')
 
     now_time = datetime.now()
-    time_str = now_time.strftime('%Y%m%d_%H%M%S')
+    time_str = now_time.strftime('%Y_%m_%d_%H_%M_%S')
+    file_dir=r=f'{TriAnswer_Record_Dir}/'+ time_str
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
     for ch_num in range(0, 3):
         if CH_EN[ch_num].get():
             if ch_num < 2:  # CH4
-                FileName = f'CH{ch_num + 1}_{time_str}({CH_SpeedText[CH_SpeedMode[ch_num].get() - 1]}).csv'
-                TA_FileHandle[ch_num] = open(os.path.join(TriAnswer_Record_Dir, FileName), "w")
+                FileName = f'CH{ch_num + 1}.csv'
+                TA_FileHandle[ch_num] = open(os.path.join(file_dir, FileName), "w")
             else:
                 # CH3
-                FileName = f'CH{ch_num + 1}_{time_str}({CH_SpeedText[3]}).csv'
-                TA_FileHandle[ch_num] = open(os.path.join(TriAnswer_Record_Dir, FileName), "w")
+                FileName = f'CH{ch_num + 1}.csv'
+                TA_FileHandle[ch_num] = open(os.path.join(file_dir, FileName), "w")
                 # CH4
-                FileName = f'CH{ch_num + 1}-2_{time_str}({CH_SpeedText[3]}).csv'
-                TA_FileHandle[ch_num + 1] = open(os.path.join(TriAnswer_Record_Dir, FileName), "w")
-
-
+                FileName = f'CH{ch_num + 2}.csv'
+                TA_FileHandle[ch_num + 1] = open(os.path.join(file_dir, FileName), "w")
+    
+    # add video file            
+    video_name = file_dir  + r'/video.avi'
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video = cv2.VideoWriter(video_name, fourcc, 30, (640, 480))
+    
 def closeRecordFile():
     global TA_FileHandle, Btn_Record_State
 
@@ -432,21 +606,24 @@ def CH_SPEED_EN(ch_num):
 def Btn_Record_Ctrl():
     global Btn_Record_State
     if Btn_Record_State == 0:
-        createRecordFile()
+        createRecordFile() #just creat file 
         Btn_Record['text'] = 'STOP\nREC'
         Btn_Record['bg'] = "#96d1b1"
-        Btn_Record_State = 1
+        start_recording = time.time()
+        Btn_Record_State = 1 # set to one start write data fo file 
+        print("set_record_state=1 time=",start_recording)
     elif Btn_Record_State == 1:
         closeRecordFile()
-
 
 if __name__ == "__main__":
 
     # Initiate TK GUI
     window = tk.Tk()
     window.title("Yutech - TriAnswer Controller")
+    window.geometry('1600x850') #注意是string，而不是數字 #inital window size
     window.resizable(width=False, height=False)
-
+    font=('Arial',15,'bold')  #字體 #width 跟 字體都有放大效果，不過要注意，如果因文字過長，物件的width不夠，會導致字無法顯示的情形喔
+    figsize=(9,6)  #繪圖大小
     # BLE block
     frm_BLE = tk.LabelFrame(master=window, text="TriAnswer Setting & Control", padx=5, pady=5)
 
@@ -513,24 +690,40 @@ if __name__ == "__main__":
     Combobox_BLE_Device.grid(row=0, column=0, padx=5, pady=5, sticky='ew')
 
     # BLE Control BTN
-    Btn_BLE_control = tk.Button(master=frm_BleCon, text="Scan", width=15,
+    Btn_BLE_control = tk.Button(master=frm_BleCon, text="Scan", width=10,font=font,
                                 bg=Btn_BLE_control_color[FLOW_CTRL_STATE], fg="yellow", command=FLOW_CTRL_BTN_action)
     Btn_BLE_control.grid(row=1, column=0, padx=5, pady=5, sticky='ew')
-
     # Record BTN
-    Btn_Record = tk.Button(master=frm_BleCon, text='Start\nREC', width=8, state='disable',
+    Btn_Record = tk.Button(master=frm_BleCon, text='Start\nREC', width=6, state='disable',font=font,
                            bg="grey", fg="white", command=Btn_Record_Ctrl)
     Btn_Record.grid(row=0, column=1, rowspan=2, padx=5, pady=5, sticky='nsew')
     Btn_Record_State = 0
 
     frm_BleCon.grid(row=1, column=0, padx=5, pady=5, sticky='ew')
 
-    frm_BLE.grid(row=0, column=0, padx=5, sticky='nsew')
-
+    frm_BLE.grid(row=0, column=1, padx=5, sticky='nsew')
+    
+    # add movie
+    #canvas_v = tk.Canvas(master=frm_BLE, width=image_width, height=image_height)  # 加入 Canvas 畫布
+    #canvas_v.place(x=0,y=0)
+    #canvas_v.grid()
+    movieLabel=tk.Label(master=frm_BLE)
+    movieLabel.grid()
+    # 先放一張logo
+    frame = cv2.imread("./logo.jpg")
+    frame = cv2.resize(frame, (image_width, image_height), interpolation=cv2.INTER_AREA)
+    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    pilImage = Image.fromarray(frame)
+    imgtk=ImageTk.PhotoImage(image=pilImage)
+    #canvas_v.create_image(0,0,anchor=NW,image=imgtk)  
+    movieLabel.config(image=imgtk)
+    movieLabel.image=imgtk
+    
+    
     # Figure block
     frm_fig = tk.LabelFrame(master=window, text="Waveform Monitor", padx=5, pady=5)
     time_array = np.arange(waveform_buffer_len) / freq
-    mat_fig, mat_ax = plt.subplots(3, 1, figsize=(5, 2), dpi=100, constrained_layout=True)
+    mat_fig, mat_ax = plt.subplots(3, 1, figsize=figsize, dpi=100, constrained_layout=True)
 
     mat_fig.patch.set_facecolor('#F0F0F0')
     mat_fig.patch.set_alpha(1.0)
@@ -548,7 +741,7 @@ if __name__ == "__main__":
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-    frm_fig.grid(row=0, column=1, padx=5, sticky='nsew')
+    frm_fig.grid(row=0, column=0, padx=5, sticky='nsew')
 
     # Log block
     frm_log = tk.LabelFrame(master=window, text="Log", padx=5, pady=5)
@@ -563,3 +756,4 @@ if __name__ == "__main__":
 
     window.protocol("WM_DELETE_WINDOW", on_WinClosing)
     window.mainloop()
+    
